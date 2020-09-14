835e004b5fafd4694b4020fcfc08f4bd202012e8
Sergey Usov <usoff82@gmail.com>
2020-09-15 00:52:54 +0600

    Update ebaybid.js

--- ebaybid.js
+++ ebaybid.js
@@ -4,7 +4,7 @@
 // @version      0.1
 // @description  try to take over the world!
 // @author       You
-// @match        https://www.tampermonkey.net/index.php?version=4.11.6117&ext=fire&updated=true
+// @match        *://*.ebay.com/*
 // @grant        none
 // ==/UserScript==
 
