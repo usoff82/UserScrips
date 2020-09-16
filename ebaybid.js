d6b9ea1db6fac43422156d24b2df9afcf8dc5bab
Sergey Usov <usoff82@gmail.com>
2020-09-16 16:14:25 +0600

    Update ebaybid.js

--- ebaybid.js
+++ ebaybid.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         My Bid
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  try to take over the world!
 // @author       You
 // @match        *://*.ebay.com/*
